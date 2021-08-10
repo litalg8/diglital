3cefc4d12facbe72740ac3d0da328a774d3954ef
litalg8 <litalgottlieb@gmail.com>
2021-08-10 18:00:38 +0300

    mobile-menu

--- src/cmps/Burger/Burger.styled.js
+++ src/cmps/Burger/Burger.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const StyledBurger = styled.button`
   position: absolute;
   top: 4%;
-  right: 3rem;
+  right: 2rem;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
